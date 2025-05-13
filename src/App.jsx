c09a045b9e6d14449d39a180150596cf9b27a38b
Antonio-pagliacci <contato.antoniopagliacci@gmail.com>
2025-05-13 14:32:47 -0300

    primeiro commit - inicio do projeto

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Antonio Pagliacci</h1>
+        <p>Tecnico em informatica - estudante - Apaixonado por tecnologia</p>
+      </header>
+      <main>
+        <h2>Sobre mim</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus et
+          distinctio nemo sit tempora. Ab quisquam mollitia vitae dolorem odio
+          omnis esse maiores quas quis! Beatae nulla quasi expedita
+          voluptatibus?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus et
+          distinctio nemo sit tempora. Ab quisquam mollitia vitae dolorem odio
+          omnis esse maiores quas quis! Beatae nulla quasi expedita
+          voluptatibus?
+        </p>
+        <h3>Habilidades Principais</h3>
+        <ul>
+          <li>Motagem e manutenção de PCs</li>
+          <li>Redes e conectividades</li>
+          <li>Sistemas Operacionais(windows, linux, MS-DOS)</li>
+          <li>Didatica técnica para cursos e certificações</li>
+        </ul>
+      </main>
+      <footer>
+        <h5>&copy; Desenvolvido por Antonio Pagliacci</h5>
+      </footer>
+    </>
+  );
+}
+
+export default App;
